547457ea8546a8732d0da7141f629f08e7c18d5a
b02202 <rwbrooks99@fullsail.edu>
2015-03-21 18:35:16 -0400

    added file storage manager

---
+++ ApplictionBarApp/app/src/main/java/com/robertbrooks/applictionbarapp/StorageManager.java
@@ -0,0 +1,41 @@
+package com.robertbrooks.applictionbarapp;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+
+/**
+ * Created by Bob on 3/21/2015.
+ */
+public class StorageManager {
+
+    public static void writeToFile(String dataString, Context context) throws FileNotFoundException {
+        try {
+            FileOutputStream fileOutStream = context.openFileOutput(dataString, Context.MODE_PRIVATE);
+            fileOutStream.write(dataString.getBytes());
+            fileOutStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String readData(String dataString, Context context) throws FileNotFoundException {
+        String results = "";
+
+
+        try {
+            FileInputStream fileInputStream = context.openFileInput(dataString);
+            results = IOUtils.toString(fileInputStream);
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return results;
+    }
+}
